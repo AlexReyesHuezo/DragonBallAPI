0647d439e60678bed37cde931c37ee45ec17ac75
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2025-04-10 16:04:50 -0600

    Create DB Context with relationships

---
+++ DragonBallAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DragonBallAPI.Models;
+
+namespace DragonBallAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Transformation> Transformations { get; set; }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Character>()
+                .HasMany(c => c.Transformations)
+                .WithOne(t => t.Character)
+                .HasForeignKey(t => t.CharacterId);
+            modelBuilder.Entity<Transformation>()
+                .HasOne(t => t.Character)
+                .WithMany(c => c.Transformations)
+                .HasForeignKey(t => t.CharacterId);
+        }
+    }
+}
